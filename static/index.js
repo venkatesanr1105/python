05db14af51fca60c9fc59650d142b94a268681b7
Venkatesan Rajendran <83106898+venkatesanr1105@users.noreply.github.com>
2022-04-08 15:29:26 +0530

    Add files via upload

---
+++ static/index.js
@@ -0,0 +1,73 @@
+localStorage.clear();
+hp.onclick = (event) => {
+	var price = event.target.previousElementSibling.textContent;
+	localStorage.setItem('product','HP');	
+	if(localStorage.getItem('productarray') && localStorage.getItem('product'))
+		localStorage.setItem('productarray',localStorage.getItem('productarray')+','+localStorage.getItem('product'));
+	else
+		localStorage.setItem('productarray',localStorage.getItem('product'));
+	if(localStorage.getItem('pricearray'))
+		localStorage.setItem('pricearray',localStorage.getItem('pricearray')+','+price.replace('Rs. ',''));
+	else
+		localStorage.setItem('pricearray',price.replace('Rs. ',''));
+		print();
+}
+
+lenovo.onclick = (event) => {
+	var price = event.target.previousElementSibling.textContent;
+	localStorage.setItem('product','Lenovo');	
+	if(localStorage.getItem('productarray') && localStorage.getItem('product'))
+		localStorage.setItem('productarray',localStorage.getItem('productarray')+','+localStorage.getItem('product'));
+	else
+		localStorage.setItem('productarray',localStorage.getItem('product'));
+	if(localStorage.getItem('pricearray'))
+		localStorage.setItem('pricearray',localStorage.getItem('pricearray')+','+price.replace('Rs. ',''));
+	else
+		localStorage.setItem('pricearray',price.replace('Rs. ',''));
+	print();
+}
+
+dell.onclick = (event) => {
+	var price = event.target.previousElementSibling.textContent;
+	localStorage.setItem('product','Dell');	
+	if(localStorage.getItem('productarray') && localStorage.getItem('product'))
+		localStorage.setItem('productarray',localStorage.getItem('productarray')+','+localStorage.getItem('product'));
+	else
+		localStorage.setItem('productarray',localStorage.getItem('product'));
+	if(localStorage.getItem('pricearray'))
+		localStorage.setItem('pricearray',localStorage.getItem('pricearray')+','+price.replace('Rs. ',''));
+	else
+		localStorage.setItem('pricearray',price.replace('Rs. ',''));
+	print();
+}
+
+apple.onclick = (event) => {
+	var price = event.target.previousElementSibling.textContent;
+	localStorage.setItem('product','Apple');	
+	if(localStorage.getItem('productarray') && localStorage.getItem('product'))
+		localStorage.setItem('productarray',localStorage.getItem('productarray')+','+localStorage.getItem('product'));
+	else
+		localStorage.setItem('productarray',localStorage.getItem('product'));
+	if(localStorage.getItem('pricearray'))
+		localStorage.setItem('pricearray',localStorage.getItem('pricearray')+','+price.replace('Rs. ',''));
+	else
+		localStorage.setItem('pricearray',price.replace('Rs. ',''));
+	print();
+}
+
+function print()
+{
+	var productArray = localStorage.getItem('productarray').split(',');
+	var priceArray = localStorage.getItem('pricearray').split(',');
+	var tableArray = document.getElementsByTagName('td');
+	var k=0;
+	for(index = 0;index<tableArray.length;index+=2)
+		tableArray[index].textContent = productArray[k++];
+	var l =0;
+	var sum = 0;
+	for(index = 1;index<tableArray.length;index+=2)
+		tableArray[index].textContent = priceArray[l++];
+	for(index=0;index<priceArray.length;index++)
+		sum+=parseInt(priceArray[index]);
+	document.getElementById('total').textContent = `Total : Rs. ${sum}`;
+}
