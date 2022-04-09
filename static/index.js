6f27a76ce82f2af961201789bcb3cc0e1185b889
Venkatesan Rajendran <83106898+venkatesanr1105@users.noreply.github.com>
2022-04-09 17:48:32 +0530

    Add files via upload

--- static/index.js
+++ static/index.js
@@ -1,21 +1,8 @@
 localStorage.clear();
-hp.onclick = function addToCart(event){
-	var price = event.target.previousElementSibling.textContent;
-	localStorage.setItem('product','HP');	
-	if(localStorage.getItem('productarray') && localStorage.getItem('product'))
-		localStorage.setItem('productarray',localStorage.getItem('productarray')+','+localStorage.getItem('product'));
-	else
-		localStorage.setItem('productarray',localStorage.getItem('product'));
-	if(localStorage.getItem('pricearray'))
-		localStorage.setItem('pricearray',localStorage.getItem('pricearray')+','+price.replace('Rs. ',''));
-	else
-		localStorage.setItem('pricearray',price.replace('Rs. ',''));
-		print();
-}
-
-lenovo.onclick = (event) => {
-	var price = event.target.previousElementSibling.textContent;
-	localStorage.setItem('product','Lenovo');	
+function addToCart(button,productName)
+{
+	var price = button.previousElementSibling.textContent;
+	localStorage.setItem('product',productName);	
 	if(localStorage.getItem('productarray') && localStorage.getItem('product'))
 		localStorage.setItem('productarray',localStorage.getItem('productarray')+','+localStorage.getItem('product'));
 	else
@@ -25,34 +12,47 @@ lenovo.onclick = (event) => {
 	else
 		localStorage.setItem('pricearray',price.replace('Rs. ',''));
 	print();
+	button.style.backgroundColor = 'Orange';
+	button.value = 'Remove Item';
+}
+
+function removeItem(button, productName)
+{
+	var regExp =new RegExp(productName);
+	console.log(regExp);
+	console.log(localStorage.getItem('productarray'));
+	console.log(regExp.test(localStorage.getItem('productarray')));
+	// localStorage.setItem('productArray',);
+}
+hp.onclick = (event) => {
+	if(event.target.value=='Add to Cart')
+		addToCart(event.target,'HP');
+	else
+		removeItem(event.target,'HP');
+		console.log('Hi');
+}
+lenovo.onclick = (event) => {
+	if(event.target.value=='Add to Cart')
+		addToCart(event.target,'Lenovo');
+	else
+	// removeElement(event.target);
+	console.log('Hi');
 }
 
 dell.onclick = (event) => {
-	var price = event.target.previousElementSibling.textContent;
-	localStorage.setItem('product','Dell');	
-	if(localStorage.getItem('productarray') && localStorage.getItem('product'))
-		localStorage.setItem('productarray',localStorage.getItem('productarray')+','+localStorage.getItem('product'));
+	if(event.target.value=='Add to Cart')
+		addToCart(event.target,'Dell');
 	else
-		localStorage.setItem('productarray',localStorage.getItem('product'));
-	if(localStorage.getItem('pricearray'))
-		localStorage.setItem('pricearray',localStorage.getItem('pricearray')+','+price.replace('Rs. ',''));
-	else
-		localStorage.setItem('pricearray',price.replace('Rs. ',''));
-	print();
+	// removeElement(event.target);
+	console.log('Hi');	
 }
 
 apple.onclick = (event) => {
-	var price = event.target.previousElementSibling.textContent;
-	localStorage.setItem('product','Apple');	
-	if(localStorage.getItem('productarray') && localStorage.getItem('product'))
-		localStorage.setItem('productarray',localStorage.getItem('productarray')+','+localStorage.getItem('product'));
+	if(event.target.value=='Add to Cart')
+		addToCart(event.target,'Apple');
 	else
-		localStorage.setItem('productarray',localStorage.getItem('product'));
-	if(localStorage.getItem('pricearray'))
-		localStorage.setItem('pricearray',localStorage.getItem('pricearray')+','+price.replace('Rs. ',''));
-	else
-		localStorage.setItem('pricearray',price.replace('Rs. ',''));
-	print();
+	// removeElement(event.target);
+	console.log('Hi');
 }
 
 function print()
@@ -61,26 +61,26 @@ function print()
 	var priceArray = localStorage.getItem('pricearray').split(',');
 	var tableArray = document.getElementsByTagName('td');
 	var k=0;
-	for(index = 0;index<tableArray.length;index+=2)
+	for(index = 0;index<tableArray.length;index+=3)
 		tableArray[index].textContent = productArray[k++];
 	var l =0;
 	var sum = 0;
-	for(index = 1;index<tableArray.length;index+=2)
+	for(index = 1;index<tableArray.length;index+=3)
 		tableArray[index].textContent = priceArray[l++];
 	for(index=0;index<priceArray.length;index++)
 		sum+=parseInt(priceArray[index]);
 	document.getElementById('total').textContent = `Total : Rs. ${sum}`;
 }
 
-function myFunc(event)
-{
-	alert('Hai');
-	var current = event.target;
-	var price = current.previousElementSibling;
-	var amount = parseInt(price.textContent);
-	price.textContent='';
-	var product = price.previousElementSibling;
-	product.textContent = "";
-	var x = document.getElementById('total').textContent
-	x.textContent = parseInt(x.textContent) - amount;
-}
+// function removeElement(event)
+// {
+// 	var current = event.target.parentElement;
+// 	var price = current.previousElementSibling;
+// 	var amount = parseInt(price.textContent);
+// 	price.textContent='';
+// 	var product = price.previousElementSibling;
+// 	product.textContent = "";
+// 	var x = document.getElementById('total').textContent.replace('Total : Rs. ','');
+// 	var total = parseInt(x) - amount
+// 	document.getElementById('total').textContent = `Total : Rs. ${total}`;
+// }
