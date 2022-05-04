e556efc579e0aa392c9893441648cbd94617f975
Venkatesan Rajendran <83106898+venkatesanr1105@users.noreply.github.com>
2022-05-04 11:04:34 +0530

    Add files via upload

---
+++ looping.py
@@ -0,0 +1,15 @@
+#break
+for n in range(10):
+	if n==7:
+		break
+	print(n,end='\t')
+#continue
+for n in range(10):
+	if n==7:
+		continue
+	print(n,end='\t')
+#pass
+for n in range(10):
+	if n==7:
+		pass
+	print(n,end='\t')
